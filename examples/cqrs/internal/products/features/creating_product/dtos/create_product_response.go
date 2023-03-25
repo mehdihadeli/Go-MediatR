d59222520337ab2196c346b43695dce625e4fc49
ehsandavari <ehsandavari.ir@gmail.com>
2023-03-25 23:43:12 +0330

    remove example

--- examples/cqrs/internal/products/features/creating_product/dtos/create_product_response.go
+++
@@ -1,7 +0,0 @@
-package dtos
-
-import uuid "github.com/satori/go.uuid"
-
-type CreateProductCommandResponse struct {
-	ProductID uuid.UUID `json:"productId"`
-}
--- examples/cqrs/internal/products/features/getting_product_by_id/dtos/get_product_by_id_response.go
+++
@@ -1,9 +0,0 @@
-package dtos
-
-import (
-	"github.com/ehsandavari/go-mediator/examples/cqrs/internal/products/dtos"
-)
-
-type GetProductByIdQueryResponse struct {
-	Product *dtos.ProductDto `json:"product"`
-}
